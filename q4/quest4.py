41f18e08128dd87a45d8ca82ccc7d40cb85c3e82
Max Barreto <maxkaibarreto@gmail.com>
2021-09-30 15:54:00 -1000

    tidying up

--- q4/quest4.py
+++ q4/quest4.py
@@ -7,14 +7,14 @@ def primes_under(n):
     return primes
 
 def is_palindromic(n):
-    is_pali = True
     n_list = [int(dig) for dig in str(n)]
     for i in range(len(n_list)>>1):
-        is_pali = is_pali and (n_list[i] == n_list[-(i+1)])
-    return is_pali
+        if not n_list[i] == n_list[-(i+1)]:
+            return False
+    return True
 
 #putting in function bc break don work
-def largest_3dig_pali():
+def largest_3dig_prime_pali():
     primes_3dig=primes_under(999)
     N = len(primes_3dig)
     p1 = p2 = 0
@@ -25,5 +25,5 @@ def largest_3dig_pali():
             if is_palindromic(p1*p2):
                 return (p1,p2)
 
-p1,p2=largest_3dig_pali()
-print("The largest palindromic product of two 3-digit primes is " + str(p1*p2) + ', the product of ' + str(p1) + ' and ' + str(p2))
+p1,p2=largest_3dig_prime_pali()
+print("The largest palindromic product of two 5-digit primes is " + str(p1*p2) + ', the product of ' + str(p1) + ' and ' + str(p2))
